b0a8541429ec1bcd48e31d2c225173701b289b35
crow <calvinje@gmail.com>
2018-07-08 12:14:36 -0700

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to letsbet
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/letsbet/core.clj
@@ -0,0 +1,75 @@
+(ns letsbet.core
+  (:gen-class))
+(require 'clojure.java.io)
+(require 'clojure.string)
+(require 'clojure.pprint)
+
+(defn -main
+  [arg]
+
+  ; Open file.
+  (with-open [rdr (clojure.java.io/reader arg)]
+
+  ; create atoms.
+  (def vertices_ (atom []))
+  (def texturecoords_ (atom []))
+  (def normals_ (atom []))
+  (def faces_ (atom []))
+
+    ; iterate over lines in file.
+    (doseq [line (line-seq rdr)]
+
+        ; :vertices
+        (when (clojure.string/includes? line "v ")
+          (def lineparse (clojure.string/split line #" "))
+          (def filtered (into [] (remove #{"v"} lineparse)))
+          (swap! vertices_ conj
+          (if (get filtered 3)
+            (sorted-map :w (get filtered 3) :x (get filtered 0) :y (get filtered 1) :z (get filtered 2))
+            (sorted-map :w 1.0 :x (get filtered 0) :y (get filtered 1) :z (get filtered 2)))
+          ))
+
+        ; :texture-coords
+        (when (clojure.string/includes? line "vt" )
+          (def lineparse (clojure.string/split line #" "))
+          (def filtered (into [] (remove #{"vt"} lineparse)))
+          (swap! texturecoords_ conj
+          (if (get filtered 2)
+            (sorted-map :w (get filtered 2) :u (get filtered 0) :v (get filtered 1))
+            (sorted-map :w 0.0 :u (get filtered 0) :v (get filtered 1)))
+          ))
+
+        ; :normals
+        (when (clojure.string/includes? line "vn" )
+          (def lineparse (clojure.string/split line #" "))
+          (def filtered (into [] (remove #{"vn"} lineparse)))
+          (swap! normals_ conj
+            (sorted-map :x (get filtered 0) :y (get filtered 1) :z (get filtered 2))
+          ))
+
+        ; :faces
+        (when (clojure.string/includes? line "f " )
+          (def lineparse (clojure.string/split line #" "))
+          (def filtered (into [] (remove #{"f"} lineparse)))
+          (swap! faces_ conj
+          (sorted-map :elements
+            (into []
+            (for [x [0 1 2]]          
+                (sorted-map :vertex (if (clojure.string/blank? (get (clojure.string/split (get filtered x) #"/") 0))
+                                      nil
+                                      (get (clojure.string/split (get filtered x) #"/") 0))
+                            :texture-coord (if (clojure.string/blank? (get (clojure.string/split (get filtered x) #"/") 1))
+                                              nil
+                                              (get (clojure.string/split (get filtered x) #"/") 1))
+                            :normal (if (clojure.string/blank? (get (clojure.string/split (get filtered x) #"/") 2))
+                                      nil
+                                      (get (clojure.string/split (get filtered x) #"/") 2)))))
+          ))))
+
+        ; build and pprint output.
+        (def finaloutput_ (hash-map
+                           :vertices (deref vertices_)
+                           :texture-coords (deref texturecoords_)
+                           :normals (deref normals_)
+                           :faces (deref faces_)))
+        (clojure.pprint/pprint finaloutput_)))
